b0f11acb646417a1e0f836e3678f0f98fae069f0
Dave Dorgan <david.dorgan@gmail.com>
2019-09-25 13:03:14 -0400

    Initial Commit

---
+++ DoToo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoToo
+//
+//  Created by David Dorgan on 9/25/19.
+//  Copyright © 2019 DED Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
