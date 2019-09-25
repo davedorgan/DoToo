c416b496bf0bd0326f7db3301fd9dc151b3d41bc
Dave Dorgan <david.dorgan@gmail.com>
2019-09-25 18:47:22 -0400

    Add new item functionality complete and deleted func tableView print statement

--- DoToo/DoTooListViewController.swift
+++ DoToo/DoTooListViewController.swift
@@ -35,7 +35,6 @@ class DoTooListViewController: UITableViewController {
     //MARK - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(indexPath.row)
 //        print(itemArray[indexPath.row])
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
