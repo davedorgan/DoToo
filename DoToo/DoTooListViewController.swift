704b86f5135e39297c3fcca3358cc2a2255af0f2
Dave Dorgan <david.dorgan@gmail.com>
2019-09-25 18:13:32 -0400

    DoToo list complete with datasource and delegate methods

---
+++ DoToo/DoTooListViewController.swift
@@ -0,0 +1,53 @@
+//
+//  DoTooListViewController.swift
+//  DoToo
+//
+//  Created by David Dorgan on 9/25/19.
+//  Copyright © 2019 DED Software. All rights reserved.
+//
+
+import UIKit
+
+class DoTooListViewController: UITableViewController {
+
+    let itemArray = ["Vacumn", "water filter", "Job App"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    // MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DoTooItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(indexPath.row)
+//        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+}
+
